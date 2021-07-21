a1de7dc9fd3c242c79c200bf3a0837f2c796a019
JhnW <jhnw.global@gmail.com>
2021-07-22 00:32:28 +0200

    chg: pkg: Package number up.

--- src/devana/_version.py
+++ src/devana/_version.py
@@ -1,5 +1,5 @@
 VERSION_MAJOR = 0  # version when they make incompatible API changes
 VERSION_MINOR = 1  # version when they add functionality in a backwards-compatible manner
-VERSION_MAINTENANCE = 1  # version when they make backwards-compatible bug fixes
+VERSION_MAINTENANCE = 2  # version when they make backwards-compatible bug fixes
 VERSION_STRING = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_MAINTENANCE}"
 __version__ = VERSION_STRING
